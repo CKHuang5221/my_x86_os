467ea69bdd64178721cff767c3747f596b93cdfc
CKHuang5221 <cutese99@gmail.com>
2024-04-21 16:04:53 +0800

    write hello world in asm

--- boot.asm
+++ boot.asm
@@ -2,12 +2,32 @@ ORG 0x7c00
 BITS 16
 
 start:
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0
-    int 0x10 ; calling BIOS interrupt
-
+    mov si, message 
+    call print
     jmp $
 
-times 510- ($ - $$) db 0
-dw 0xAA55   ;remember intel use little endian
+print:
+    mov bx, 0 
+
+.loop:
+    lodsb ; load addr to 'al' where 'si' pointing and increase si
+    cmp al, 0; compare al == 0 ?
+    je .done    ; jump equal
+    call print_char
+    jmp .loop
+.done:
+    ret
+
+print_char:
+
+    mov ah, 0eh
+    int 0x10 ; calling BIOS interrupt
+    ret
+
+message: db 'Hello World!' , 0 ;use 'db' for char or string
+
+times 510 - ($ - $$) db 0   ; $ means current addr, $$ means segment addr
+;dw 0xAA55   ;remember intel use little endian
+db 0x55
+db 0xAA
+

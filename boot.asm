9db4a5ba3a424187234c338676f0ab9d6cc05d1f
CKHuang5221 <cutese99@gmail.com>
2024-04-22 21:36:13 +0800

    change org to 0

--- boot.asm
+++ boot.asm
@@ -1,13 +1,22 @@
-ORG 0x7c00
+ORG 0
 BITS 16
 
 start:
+    cli ;disable  interrups
+    mov ax, 0x7c0
+    mov ds, ax
+    mov es, ax
+    mov ax, 0x00
+    mov ss, ax
+    mov sp, 0x7c00
+    sti ;enable interrups
+
     mov si, message 
     call print
     jmp $
 
 print:
-    mov bx, 0 
+    mov bx, 0 ; position , where to display
 
 .loop:
     lodsb ; load addr to 'al' where 'si' pointing and increase si

4c1e6ac621c2a34420f027750bb824da89e8a172
CKHuang5221 <cutese99@gmail.com>
2024-04-23 15:35:20 +0800

    read message using chs

--- boot.asm
+++ boot.asm
@@ -10,13 +10,6 @@ times 33 db 0
 start:
     jmp 0x7c0:step2 ;set code segment to 0x7c0
 
-my_int0:    ;create our own interrupt
-    mov ah, 0eh
-    mov al, 'A'
-    mov bx, 0x00
-    int 0x10
-    iret
-
 step2:
     ;Dont rely BIOS set 0x7c0 for us
     cli ;disable  interrups
@@ -28,18 +21,27 @@ step2:
     mov sp, 0x7c00
     sti ;enable interrups
 
-    mov word[ss:0x00], my_int0
-    mov word[ss:0x02], 0x7c0
+    mov ah, 2
+    mov al, 1
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov bx, buffer
+    int 0x13
+    jc error    ; if failed
 
-    int 0
+    mov si, buffer
+    call print
 
-    mov si, message 
+    jmp $
+
+error:
+    mov si, error_message
     call print
     jmp $
 
 print:
     mov bx, 0 ; position , where to display
-
 .loop:
     lodsb ; load addr to 'al' where 'si' pointing and increase si
     cmp al, 0; compare al == 0 ?
@@ -55,10 +57,12 @@ print_char:
     int 0x10 ; calling BIOS interrupt
     ret
 
-message: db 'Hello World!' , 0 ;use 'db' for char or string
+error_message: db 'failed to load sector'
 
 times 510 - ($ - $$) db 0   ; $ means current addr, $$ means segment addr
 ;dw 0xAA55   ;remember intel use little endian
 db 0x55
 db 0xAA
 
+
+buffer:

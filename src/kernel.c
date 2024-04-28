fe9ba327e8334eb0e985a795c9e880dc9f63514c
CKHuang5221 <cutese99@gmail.com>
2024-04-28 22:06:00 +0800

    create our print function

--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,70 @@
 #include "kernel.h"
+#include <stdint.h> //for uint16_t
+#include <stddef.h> //for size_t
+
+uint16_t* video_mem = 0;
+int terminal_col = 0;
+int terminal_row = 0;
+
+uint16_t terminal_make_char(char c, char color){
+    return (color << 8) | c;    //combine character and color to 16 bits
+}
+
+void terminal_put_char(int x, int y, char c, char color){
+    video_mem[ (y * VGA_WIDTH) + x ] = terminal_make_char(c, color);
+}
+
+void terminal_write_char(char c, char color){
+    if( c == '\n'){
+        terminal_row = terminal_row + 1; //next line
+        terminal_col = 0;
+        return;
+    }
+    
+    terminal_put_char(terminal_col, terminal_row, c, color);
+    terminal_col = terminal_col + 1 ;
+
+    if(terminal_col >= VGA_WIDTH){  //next line
+        terminal_col = 0;
+        terminal_row = terminal_row + 1;
+    }
+}
+
+
+void terminal_init(){   //clear screen to all blank
+    terminal_col = 0;
+    terminal_row = 0;
+    video_mem = (uint16_t*)(0xB8000);
+
+    for(int y = 0; y < VGA_HEIGHT; y++){
+        for(int x = 0; x < VGA_WIDTH; x++){
+            terminal_put_char(x, y, ' ', 0 );
+        }
+    }
+
+}
+
+size_t strlen( const char* str ){
+    size_t len = 0;
+
+    while( str[len] ){
+        len++;
+    }
+
+    return len;
+}
+
+void print(const char* str){
+    int len = strlen(str);
+    for(int i = 0; i<len; i++){
+        terminal_write_char(str[i], 1);
+    }
+}
 
 void kernel_main(){
-
+    
+    terminal_init();
+    //video_mem[0] = 0x0341;  //remember little endian
+    //terminal_write_char('Q', 2);
+    print("abc \n efg");
 }
